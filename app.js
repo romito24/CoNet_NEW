ea685f7a8906f827a9853dd96760505769a81d38
romito24 <romito@mta.ac.il>
2025-11-23 21:44:15 +0200

    קבצים בסיסיים כדי שהשרת ירוץ

---
+++ app.js
@@ -0,0 +1,21 @@
+require('dotenv').config(); 
+
+const express = require('express'); 
+
+const mysql = require('mysql2/promise'); 
+
+const app = express(); 
+app.use(express.json()); 
+
+const db = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10 
+});
+
+app.listen(3000);
+
+module.exports = { app, db };
