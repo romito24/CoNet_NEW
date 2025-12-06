738be2203dfa4b32a99b3cd06f1781cc3002879b
romito24 <romito@mta.ac.il>
2025-12-06 20:04:08 +0200

    הוספת API אירועים

--- app.js
+++ app.js
@@ -3,16 +3,18 @@ const express = require('express');
 const app = express(); 
 
 const authRoutes = require('./routes/auth'); 
-const spaceRoutes = require('./routes/spaces'); 
-const orderRoutes = require('./routes/orders'); 
+const spaceRoutes = require('./routes/spaces');
+const { router: orderRoutes } = require('./routes/orders'); 
 const communityRoutes = require('./routes/communities');
+const eventRoutes = require('./routes/events'); 
 
 app.use(express.json()); 
 
 app.use('/api/auth', authRoutes);
-app.use('/api/spaces', spaceRoutes); 
-app.use('/api/orders', orderRoutes); 
+app.use('/api/spaces', spaceRoutes);
+app.use('/api/orders', orderRoutes);
 app.use('/api/communities', communityRoutes);
+app.use('/api/events', eventRoutes); 
 
 app.get('/', (req, res) => {
     res.send('CONET Server is running correctly!');
@@ -20,5 +22,5 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`🚀 Server is running on port ${PORT}`);
 });
