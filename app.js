108e6fc44eddff14a1d20770b9488bba457c031a
romito24 <romito@mta.ac.il>
2026-01-11 00:06:57 +0200

    ביטול שינוי

--- app.js
+++ app.js
@@ -94,11 +94,6 @@ app.get('/add_community', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'new_community.html'));
 });
 
-// דף מרחבים
-app.get('/spaces', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'search.html'));
-});
-
 // הפעלת השרת
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
